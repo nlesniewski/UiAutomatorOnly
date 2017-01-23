81e58283c56103b80648bcbc426a83bd9eae8084
Norbert Leśniewski <norbert.lesniewski@intive.com>
2017-01-23 15:31:37 +0100

    renamed example test file and created test for wake and sleep device

--- app/src/androidTest/java/it/lesniewski/uiautomatoronly/MinimalUiAutomatorTest.java
+++ app/src/androidTest/java/it/lesniewski/uiautomatoronly/MinimalUiAutomatorTest.java
@@ -3,6 +3,7 @@ package it.lesniewski.uiautomatoronly;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +16,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class MinimalUiAutomatorTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void checkContextsAndManipulateDevice() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("it.lesniewski.uiautomatoronly", appContext.getPackageName());
+
+        Context testAppContext = InstrumentationRegistry.getContext();
+        assertEquals("it.lesniewski.uiautomatoronly.test",  testAppContext.getPackageName());
+
+        UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        uiDevice.wakeUp();
+        uiDevice.pressMenu();
+        uiDevice.pressHome();
+        uiDevice.sleep();
     }
 }
